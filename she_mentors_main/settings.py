4c58c2f27726ee1adc499762edefcc2621ce0154
Joha-will <johawilsonatu@icloud.com>
2025-03-21 20:34:35 +0000

    updating tailwind dependencies

--- she_mentors_main/settings.py
+++ she_mentors_main/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'she_mentors_main.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,9 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATICFILES_DIRS = [BASE_DIR / "static"]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
