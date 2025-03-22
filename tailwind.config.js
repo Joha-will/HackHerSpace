5f6e6f32837c26662457c58297c935ce1e213492
Mubashirgit1 <mubashir.connect@gmail.com>
2025-03-22 16:13:26 +0000

    start work on navbar and footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,9 @@ module.exports = {
   content: [
     'templates/*.html',
     './home/templates/home/*.html',
-
+'./templates/**/*.html',
+    './**/templates/**/*.html',
+    './static/js/**/*.js'
   ],
   theme: {
     extend: {},
