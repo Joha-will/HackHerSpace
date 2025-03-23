1825f94e5cb0cb847a5fbd3af4f54878a60661c9
Mubashirgit1 <mubashir.connect@gmail.com>
2025-03-23 16:21:14 +0000

    changes come from main branch

--- users/views.py
+++ users/views.py
@@ -1,37 +1,36 @@
 from django.shortcuts import redirect, render, reverse
 from django.contrib.auth import login, authenticate
-from django.urls import reverse_lazy
-from django.views import generic
-from django.contrib.auth.models import User, auth
-from .forms import SignInForm, UserSignUpForm
+from django.contrib import messages, auth
+from django.contrib.auth.models import User
+from .forms import UserForm
 
-"""
-# Create your views here.
-class ProfileList(generic.ListView):
-    model = User
-    template_name = "users/profile_list.html"  # Ensure this template exists
 
-"""
 
 def sign_up(request):
     try:
         if request.user.is_authenticated:
             return redirect('home')
     
+        elif request.method == 'POST':
+
+            form = UserForm(request.POST)
+            if form.is_valid():
+                password = form.cleaned_data['password']
+                user = form.save(commit=False)
+                user.set_password(password)
+                user.role = User.STUDENT
+                user.save()
+                messages.success(request, 'Your account has been registered successfully!')
+                return redirect('register_user')
         else:
-            if request.method == "POST":
-                form = UserSignUpForm(request.POST)
-                if form.is_valid():
-                    user = form.save()
-                    auth.login(request, user)
-                    return redirect(reverse('sign_in'))
-            else:
-                form = UserSignUpForm()
+            print('invalid form')
+            print(form.errors)
     except Exception as e:
         print(f"Error occurred while signing up: {e}")
-        form = UserSignUpForm()  # Re-render form with error messages for user to correct.
-
-    context = { 'form': form,}
+        form = UserForm()
+    context = {
+        'form': form,
+    }
 
     return render(request, 'users/registration/sign_up.html', context)
 
@@ -41,17 +40,11 @@ def sign_in(request):
         if request.user.is_authenticated:
             return redirect('home')
         
-        else:
-            
-            form = SignInForm()
+        elif request.method == 'POST':
+                username = request.POST['username']
+                password = request.POST['password']
 
-            if request.method == "POST":
-                form = SignInForm(request, data=request.POST)
-                if form.is_valid():
-                    username = request.POST.get('username')
-                    password = request.POST.get('password')
-
-                user = authenticate(request, username=username, password=password)
+                user = auth.authenticate(username=username, password=password)
 
                 if user is not None:
                     auth.login(request, user)
@@ -61,13 +54,23 @@ def sign_in(request):
 
     except Exception as e:
         print(f"Error occurred while signing in: {e}")
-        form = SignInForm()
-    
-    context = { 'form': form,}
 
-    return render(request, 'users/registration/sign_in.html', context)
+    return render(request, 'users/registration/sign_in.html')
 
 
 def sign_out(request):
     auth.sign_out(request)
     return redirect('home')
+
+
+
+
+
+
+"""
+# Create your views here.
+class ProfileList(generic.ListView):
+    model = User
+    template_name = "users/profile_list.html"  # Ensure this template exists
+
+"""
