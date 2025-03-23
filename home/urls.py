d08ab936f63123d93b6e5c3ce12d36e9e671d720
Anthony Nelson <anthonyjnelson@hotmail.com>
2025-03-23 00:15:43 +0000

    create main women in tech layout

--- home/urls.py
+++ home/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),  # Home page
-    path('women-in-tech/', views.women_tech, name='women_tech'),  # Women in Tech page
+    path('women_in_tech/', views.women_in_tech, name='women_in_tech'),  # Women in Tech page
     path('team/', views.team, name='team'),  # Team page
 ]
--- home/views.py
+++ home/views.py
@@ -7,7 +7,7 @@ def home(request):
     '''
     return render(request, 'home/index.html')
 
-def women_tech(request):
+def women_in_tech(request):
     '''
     Render women in tech page
     '''
