a90e9d39d4312e6cb507ce0b667eed2c41b75394
Joha-will <johawilsonatu@icloud.com>
2025-03-23 02:19:01 +0000

    created user manage and User models, views

--- users/forms.py
+++ users/forms.py
@@ -1,31 +1,28 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.auth.models import User
+from .models import User
 
 
 
 
-class UserSignUpForm(UserCreationForm):
-
-    class Meta:
-        model = User
-        fields = ('username', 'password1', 'password2')
-
-    
-    def save(self, commit=True):
-        user = super().save(commit=False)
-        if commit:
-            user.save()
-        return user
-
-
-
-class SignInForm(AuthenticationForm):
-    username = forms.CharField(
-        label="Username",
-        widget=forms.TextInput()
-    )
+class UserForm(forms.ModelForm):
     password = forms.CharField(
         label="Password",
         widget=forms.PasswordInput()
     )
+    confirm_password = forms.CharField(
+        label="Confirm Password",
+        widget=forms.PasswordInput()
+    )
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email', 'password',)
+
+    
+    def clean(self):
+        cleaned_data = super(UserForm, self).clean()
+        password = cleaned_data.get('password')
+        confirm_password = cleaned_data.get('confirm_password')
+
+        if password and confirm_password:
+            raise forms.ValidationError("Passwords do not match.")
--- users/models.py
+++ users/models.py
@@ -3,6 +3,44 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.core.exceptions import ValidationError
 
 
+
+class UserManager(BaseUserManager):
+
+    def create_user(self, username, first_name, last_name, email, password=None):
+        if not username:
+            raise ValidationError('Username is required.')
+        if not email:
+            raise ValidationError('Email is required.')
+    
+        user = self.model(
+            username=username,
+            first_name=first_name,
+            last_name=last_name,
+            email=self.normalize_email(email),
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, username, first_name, last_name, email, password):
+        user = self.create_user(
+
+            email = self.normalize_email(email),
+            username = username,
+            password = password,
+            first_name = first_name,
+            last_name = last_name,
+        )
+        user.is_admin = True
+        user.is_active = True
+        user.is_staff = True
+        user.is_superadmin = True
+        user.save(using=self._db)
+        return user
+    
+
+
 class User(AbstractBaseUser):
 
     MENTOR = 1
@@ -21,12 +59,23 @@ class User(AbstractBaseUser):
     last_login = models.DateTimeField(auto_now=True)
     created_date = models.DateTimeField(auto_now=True)
     updated_date = models.DateTimeField(auto_now=True)
+    is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=False)
+    is_superadmin = models.BooleanField(default=False)
 
     USERNAME_FIELD = 'username'
     REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+    objects = UserManager()
 
     def __str__(self):
-        return self.username
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        return self.is_admin
+    
+    def has_module_perms(self, app_label):
+        return True
     
     def get_role(self):
         if self.role == 1:
