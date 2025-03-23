a2b45d084d5060a362dc37a7c11624dc89a0c288
Mubashirgit1 <mubashir.connect@gmail.com>
2025-03-23 16:06:47 +0000

    complete about, mentor

--- home/urls.py
+++ home/urls.py
@@ -5,5 +5,8 @@ urlpatterns = [
     path('', views.home, name='home'),  # Home page
     path('women-in-tech/', views.women_tech, name='women_tech'),  # Women in Tech page
     path('contact/', views.contact, name='contact'),  # Team page
+    path('about/', views.about, name='about'),  # Team page
     path('questions/', views.questions, name='questions'),  # Questions page
+    path('mentor/', views.mentor, name='mentor'),  # Mentor page
+    
 ]
